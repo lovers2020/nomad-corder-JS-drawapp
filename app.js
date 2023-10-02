fe5e5ce37705b9c132caccec445753e31df50a55
lovers2020 <dkrkskswl@naver.com>
2023-10-02 23:02:45 +0900

    Update file

---
+++ app.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = 2;
+
+let isPainting = false;
+ctx.moveTo(0, 0);
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting(event) {
+  isPainting = true;
+}
+
+function cancelPainting(event) {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
